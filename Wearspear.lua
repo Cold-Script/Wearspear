a0445927530a634cd05830ad6b9e0a8291751401
rechedYR <166982379+Cold-Script@users.noreply.github.com>
2024-11-30 06:03:46 +0700

    Update Wearspear.lua

--- Wearspear.lua
+++ Wearspear.lua
@@ -1,1 +1,4 @@
-
+local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/weakhoes/Roblox-UI-Libs/refs/heads/main/Vanis Lib/Vanis Lib Source.lua'))()
+local Window = library:CreateWindow("Wearspear", "Thanks : " .. game.Players.LocalPlayer.Name .. " | Your ID : " .. game.Players.LocalPlayer.UserId, 10044538000)
+local Tab = Window:CreateTab("Select")
+local Page = Tab:CreateFrame("Page 1")
